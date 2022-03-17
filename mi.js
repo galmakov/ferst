f6a7920063c9d16008c4e4794b14d6fcace99ca7
Yurij <galmakov@gmail.com>
2022-03-17 14:50:08 +0200

    first commin

---
+++ mi.js
@@ -0,0 +1,39 @@
+'use strict';
+
+
+let persone = {
+    name : "John",
+    age: 25,
+    isMarried: false 
+};
+
+console.log(persone["name"]);
+
+let arr = ['plum.png','orange.jpg','apple.bmp'];
+console.log(arr[0]);
+
+// let answer = confirm("Are you here?");
+
+// console.log(answer);
+
+// let answer = prompt("Вам есть 18 лет?", "Да");
+
+// console.log(typeof(answer));
+
+// console.log("arr" + " - object");
+// console.log(4 + +" - object");
+
+let incr = 10,
+    decr = 10;
+
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log("2" == 2);
+
+let isChecket = false,
+    isClose = false;
+
+console.log(isChecket || !isClose);
